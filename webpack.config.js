736fbd5c9382fcc13840a07f5e21d0b0394449a7
Matheus Souza Silva <47762135+matssilva@users.noreply.github.com>
2019-07-03 10:56:37 -0300

    Initial App
    Initial App

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
+
+module.exports = {
+  entry: {
+    'root-application': 'src/root-application/root-application.js',
+    'common-dependencies': [
+      // We want just one version of angular, so we put it into the common dependencies
+      'core-js/client/shim.min.js',
+      '@angular/common',
+      '@angular/compiler',
+      '@angular/core',
+      '@angular/platform-browser-dynamic',
+      '@angular/router',
+      'reflect-metadata',
+      /* Just one version of react, too. react-router is fine to have multiple versions of,
+       * though, so no need to put it in common dependencies
+       */
+      'react',
+      'react-dom',
+    ],
+  },
+  output: {
+    publicPath: '/dist/',
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+        test: /\.js?$/,
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  node: {
+    fs: 'empty'
+  },
+  resolve: {
+    modules: [
+      __dirname,
+      'node_modules',
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      name: 'common-dependencies.js',
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new ContextReplacementPlugin(
+      /(.+)?angular(\\|\/)core(.+)?/,
+      path.resolve(__dirname, '../src')
+    )
+  ],
+  devtool: 'source-map',
+  externals: [],
+  devServer: {
+    historyApiFallback: true
+  }
+};
